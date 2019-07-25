59c71aa8efdd33e45f85dfb580ab746609260d06
Michael Kuenzli <michael.kuenzli@camptocamp.com>
2019-07-25 09:12:00 +0200

    Add example

---
+++ example.js
@@ -0,0 +1,9 @@
+const ExclusiveKeyboard = require('./index');
+
+const keyboard = new ExclusiveKeyboard('by-id/usb-Logitech_Logitech_USB_Keyboard-event-kbd');
+keyboard.on('keyup', console.log);
+keyboard.on('keydown', console.log);
+keyboard.on('keypress', console.log);
+keyboard.on('error', console.error);
+
+console.log(keyboard.Keys);
